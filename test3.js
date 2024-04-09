7c82bd5af7c5b5c180867c96834851c5bef74e64
Cathy <cathy96055@gmail.com.tw>
2024-04-09 10:43:43 +0800

    test

---
+++ test3.js
@@ -0,0 +1,38 @@
+class student {
+    name;
+    scorea;
+    scoreb;
+    constructor(name, scorea, scoreb) {
+        this.name = name;
+        this.scorea = scorea;
+        this.scoreb = scoreb;
+        this.isgood = scorea >= 60 && scoreb >= 60
+    }
+    sco() {
+        const score2 = sa.filter(item => item.scorea >= 60 && item.scoreb >= 60);
+
+    }
+    // get isgood() {
+    //     return this.scorea >= 60 && this.scoreb >= 60
+    //     // if (this.scorea >= 60 && this.scoreb >= 60) {
+    //     //     return true;
+    //     // }
+    //     // else {
+    //     //     return false;
+    //     // }
+    // }
+
+}
+let studenta = new student("123", 90, 80);
+let studentb = new student("456", 10, 40);
+let studentc = new student("789", 60, 70);
+
+// console.log(studenta);
+
+let arr = [studenta, studentb, studentc];
+
+const sa = arr.filter(item => item.scorea >= 60 && item.scoreb >= 60);
+// console.log(sa[0].name);
+const sa2 = arr.filter(item => item.isgood)
+
+console.log(sa2)
